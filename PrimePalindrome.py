6e5f06c18033c3408b196f5b00a330e513099c2b
Kevin McIntosh <kevjames3@gmail.com>
2014-05-21 23:22:04 -0700

    Added isPrime method and tested for Prime numbers up to 1000

---
+++ PrimePalindrome.py
@@ -0,0 +1,34 @@
+'''
+Focus of this file is to determine the biggest prime palindrome under 1000.
+'''
+import random
+import math
+
+def isPrime(num):
+	result = True
+	if num > 2 and not (num % 2 == 0):
+		#Use Fermat's little theorem a^n (mod n) = a (mod n).
+		#If they equal each other, then that means that this 
+		#one is prime.  Also, 2 <= a < n where a is an integer 
+		a = random.randint(2, num - 1)
+		if ((a ** num) % num) == (a % num):
+			sqrt_n = math.sqrt(num)
+			for divider in range(3, int(math.ceil(sqrt_n) + 1)):
+				if num % divider == 0:
+					result = False
+					break
+		else:
+			result = False
+
+	elif not num == 2:
+		result = False 
+	
+	return result
+
+if __name__ == '__main__':
+	primeList = []
+	for num in range(0, 1001):
+		if isPrime(num):
+			primeList.append(num)
+	
+	print primeList 
