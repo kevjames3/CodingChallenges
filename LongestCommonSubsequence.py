df877ab773797a36b9ae71e0e14533217d181544
Kevin McIntosh <kevjames3@gmail.com>
2014-11-22 18:22:15 -0800

    Ground work laid for longest common subsequence

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -1,9 +1,24 @@
-import random
-import math
+import sys
 
 def longestCommonSubsequence(x, y):
-    result = []
-    return result
+    return x
 
 if __name__ == '__main__':
-    print "Hello World"
+    if(len(sys.argv) != 2 and len(sys.argv) != 3):
+        print "Incorrect command line arguments"
+        print "Useage: LongestCommonSubsequence.py [File] [?'Testing']"
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    testing = (True if sys.argv[2] == "Testing" else False)  
+
+    fileHandle = open(filepath, 'r')
+    for line in fileHandle:
+        test, answer = map(str.strip, line.split("#"))  
+        x,y = map(str.strip, test.split(";"))
+        if testing:
+            print "Output: '%s', Answer: '%s'" % (longestCommonSubsequence(x, y), answer)
+        else:
+            print longestCommonSubsequence(x, y)
+
+    sys.exit(0)
