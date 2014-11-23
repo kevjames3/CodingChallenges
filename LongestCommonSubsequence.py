ed18f6613bcf5bb5b21208d5e2bbea707c6d3deb
Kevin McIntosh <kevjames3@gmail.com>
2014-11-22 19:47:53 -0800

    Added a debugging statement

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -8,6 +8,8 @@ def longestCommonSubsequence(x, y):
     if len(x) == 0 or len(y) == 0:
         return ""
 
+    print "X: '%s', Y: '%s' " % (x,y)
+
     x_i = x[-1]
     y_i = y[-1]
 
@@ -21,6 +23,19 @@ def longestCommonSubsequence(x, y):
         else:
             return result_y_minus_one
 
+def processFile(fileHandle):
+    for line in fileHandle:
+        if not line.strip():
+            continue;
+        
+        x,y = map(str.strip, line.split(";"))
+
+        if len(x) > 50 or len(y) > 50:
+            #This is based on the requirement in the document
+            continue
+
+        print longestCommonSubsequence(x, y)
+
 if __name__ == '__main__':
     if(len(sys.argv) != 2 and len(sys.argv) != 3):
         print "Incorrect command line arguments"
@@ -31,19 +46,5 @@ if __name__ == '__main__':
     testing = (True if len(sys.argv) == 3 and sys.argv[2] == "Testing" else False)  
 
     fileHandle = open(filepath, 'r')
-    for line in fileHandle:
-        if line.strip() == "":
-            continue;
-        
-        x,y = map(str.strip, line.split(";"))
-
-        if len(x) > 50 or len(y) > 50:
-            #This is based on the requirement in the document
-            continue
-
-        if testing:
-            print "Output: '%s', Answer: '%s'" % (longestCommonSubsequence(x, y), answer)
-        else:
-            print longestCommonSubsequence(x, y)
-
-    sys.exit(0)
+    processFile(fileHandle)
+    fileHandle.close()
